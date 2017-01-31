24f5fc4a68d17dd342a7201da44de885b5ac8a8b
Richard Brown <richard.brown@incrementalgroup.co.uk>
2017-01-31 16:07:47 +0000

    Added .js taken from mygov.scot site (so that search box is responsive, and cookie message is displayed)

--- SgAlpha2/FormUI/App_Start/BundleConfig.cs
+++ SgAlpha2/FormUI/App_Start/BundleConfig.cs
@@ -7,11 +7,10 @@ namespace FormUI
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
+            bundles.Add(new ScriptBundle("~/bundles/scripts").Include(
+                        "~/Scripts/jquery-{version}.js",
+                        "~/Scripts/jquery.validate*",
+                        "~/Scripts/mygov.scot.global.js"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
