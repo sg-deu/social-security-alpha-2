e57915954ec91d65aaeb80e4209dbe90fa489be9
Richard Brown <richard.brown@incrementalgroup.co.uk>
2017-02-01 15:36:21 +0000

    Added Next button to start BSG form

---
+++ SgAlpha2/FormUI/Controllers/Helpers/HtmlHelpers.cs
@@ -0,0 +1,30 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace FormUI.Controllers.Helpers
+{
+    public static class HtmlHelpers
+    {
+        public static UrlHelper UrlHelper<T>(this HtmlHelper<T> helper)
+        {
+            return new UrlHelper(helper.ViewContext.RequestContext);
+        }
+
+        public static IHtmlString ButtonLinkNext<T>(this HtmlHelper<T> helper, string actionUrl)
+        {
+            return helper.ButtonLink(actionUrl, "Next");
+        }
+
+        public static IHtmlString ButtonLink<T>(this HtmlHelper<T> helper, string actionUrl, string text)
+        {
+            var urlHelper = helper.UrlHelper();
+
+            var html =
+                $"<a class=\"button button--primary\" title=\"Next\" href=\"{urlHelper.Content(actionUrl)}\">"
+                    + $"<span class=\"link-text\">{text}</span>"
+                  + "</a>";
+
+            return new MvcHtmlString(html);
+        }
+    }
+}
