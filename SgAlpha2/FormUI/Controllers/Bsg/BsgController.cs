21fc7dfc70394e24723a0cfc398d6977f15a0c4a
Richard Brown <richard.brown@incrementalgroup.co.uk>
2017-02-01 15:07:11 +0000

    Added landing page for BSG (Best Start Grant)

---
+++ SgAlpha2/FormUI/Controllers/Bsg/BsgController.cs
@@ -0,0 +1,25 @@
+﻿using System.Web.Mvc;
+
+namespace FormUI.Controllers.Bsg
+{
+    public static class BsgActions
+    {
+        public static string    Index()     { return "~/bsg"; }
+        public static string    Overview()  { return "~/bsg/overview"; }
+    }
+
+    public class BsgController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Overview()
+        {
+            return View();
+        }
+    }
+}
