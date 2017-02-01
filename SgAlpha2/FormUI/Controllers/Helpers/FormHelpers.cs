7c2c3a1e701050d605e1229c5c61697a2f2f002f
Richard Brown <richard.brown@incrementalgroup.co.uk>
2017-02-01 14:46:51 +0000

    Pushed submit button into helper

---
+++ SgAlpha2/FormUI/Controllers/Helpers/FormHelpers.cs
@@ -0,0 +1,23 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace FormUI.Controllers.Helpers
+{
+    public static class FormHelpers
+    {
+        public static IHtmlString ButtonSubmit<T>(this HtmlHelper<T> helper)
+        {
+            return helper.ButtonSubmit("Submit");
+        }
+
+        public static IHtmlString ButtonSubmit<T>(this HtmlHelper<T> helper, string text)
+        {
+            var html =
+                "<div class=\"form-group\">"
+                    + $"<button data-gtm=\"form-submit\" class=\"button button--primary\">{text}</button>"
+                + "</div>";
+
+            return new MvcHtmlString(html);
+        }
+    }
+}
