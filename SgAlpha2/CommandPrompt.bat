b50655fde948a5fddcc53a4274c37799b1d550ea
Richard Brown <richard.brown@incrementalgroup.co.uk>
2017-02-03 08:21:04 +0000

    Added release configuration to local packaging

--- SgAlpha2/CommandPrompt.bat
+++ SgAlpha2/CommandPrompt.bat
@@ -2,6 +2,6 @@
 @title SgAlpha2 Command Prompt
 @SET PATH=C:\Program Files (x86)\MSBuild\14.0\Bin\;%PATH%
 @doskey b=msbuild $* SgAlpha2.proj
-@doskey bPackage=msbuild /p:DeployOnBuild=true /p:WebPublishMethod=Package /p:PackageAsSingleFile=true /p:SkipInvalidConfigurations=true /p:PackageLocation="%CD%\azurePackage" SgAlpha2.sln
+@doskey bPackage=msbuild /p:Configuration=Release /p:DeployOnBuild=true /p:WebPublishMethod=Package /p:PackageAsSingleFile=true /p:SkipInvalidConfigurations=true /p:PackageLocation="%CD%\azurePackage" SgAlpha2.sln
 type readme.txt
 %comspec%
