52f661103baf8d11c694739b25d56478b918fc3f
Richard Brown <richard.brown@incrementalgroup.co.uk>
2017-02-03 12:04:03 +0000

    Added input name

--- SgAlpha2/FormUI/Controllers/Helpers/FormHelpers.cs
+++ SgAlpha2/FormUI/Controllers/Helpers/FormHelpers.cs
@@ -26,9 +26,11 @@ namespace FormUI.Controllers.Helpers
 
         public static IHtmlString LabelledInputText<T>(this HtmlHelper<T> helper, string labelText, Expression<Func<T, string>> property, string hintText = null)
         {
-            var label = new HtmlTag("label").Text(labelText);
+            var name = GetExpressionName(helper, property);
 
-            var input = new HtmlTag("input").Attr("type", "text").AddClasses("form-control");
+            var label = new HtmlTag("label").Text(labelText).Attr("for", name);
+
+            var input = new HtmlTag("input").Attr("type", "text").AddClasses("form-control").Id(name).Name(name);
 
             var inputWrapper = new DivTag().AddClasses("input-wrapper").Append(input);
 
@@ -40,10 +42,40 @@ namespace FormUI.Controllers.Helpers
             if (!string.IsNullOrWhiteSpace(hintText))
             {
                 var hint = new HtmlTag("p").AddClasses("help-block").AppendHtml(hintText);
-                label.After(hint);
+                label.Append(hint);
             }
 
             return formGroup;
+        }
+
+        private static string GetExpressionName<T>(HtmlHelper<T> helper, LambdaExpression expression)
+        {
+            return GetExpressionText(expression.Body);
+        }
+
+        private static string GetExpressionText(LambdaExpression expression)
+        {
+            return GetExpressionText(expression.Body);
+        }
+
+        private static string GetExpressionText(Expression expression)
+        {
+            var me = GetMemberExpression(expression);
+            return me.Member.Name;
+        }
+
+        private static MemberExpression GetMemberExpression(Expression expression)
+        {
+            if (expression.NodeType == ExpressionType.MemberAccess)
+                return (MemberExpression)expression;
+
+            if (expression.NodeType == ExpressionType.Convert || expression.NodeType == ExpressionType.ConvertChecked)
+            {
+                var ue = (UnaryExpression)expression;
+                return GetMemberExpression(ue.Operand);
+            }
+
+            throw new Exception(string.Format("Could not determine expression for member {0} of type {1}", expression, expression.NodeType));
         }
     }
 }
