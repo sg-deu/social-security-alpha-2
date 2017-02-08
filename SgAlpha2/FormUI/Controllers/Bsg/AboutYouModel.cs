42a5fb961d6b94aea265caf238f1a5e86f4b2acb
Richard Brown <richard.brown@incrementalgroup.co.uk>
2017-02-08 10:23:36 +0000

    Fleshed out some more model properties (need to move this into a Domain folder)

--- SgAlpha2/FormUI/Controllers/Bsg/AboutYouModel.cs
+++ SgAlpha2/FormUI/Controllers/Bsg/AboutYouModel.cs
@@ -1,19 +1,44 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace FormUI.Controllers.Bsg
 {
     public class AboutYouModel
     {
-        public string   Title                       { get; set; }
+        public AboutYouModel()
+        {
+            PreviousAddresses = new List<AddressModel>();
+        }
 
-        public string   FirstName                   { get; set; }
+        public string       Title                       { get; set; }
 
-        public string   OtherNames                  { get; set; }
+        public string       FirstName                   { get; set; }
 
-        public string   SurnameOrFamilyName         { get; set; }
+        public string       OtherNames                  { get; set; }
 
-        public DateTime DateOfBirth                 { get; set; }
+        public string       SurnameOrFamilyName         { get; set; }
 
-        public string   NationalInsuranceNumberText { get; set; }
+        public DateTime     DateOfBirth                 { get; set; }
+
+        public string       NationalInsuranceNumberText { get; set; }
+
+        public AddressModel CurrentAddress              { get; set; }
+
+        public bool?        CurrentAddressIsPermanent   { get; set; }
+
+        public IList<AddressModel> PreviousAddresses { get; set; }
+    }
+
+    public class AddressModel
+    {
+        public string   Street1     { get; set; }
+
+        public string   Street2     { get; set; }
+
+        public string   TownOrCity  { get; set; }
+
+        public string   Postcode    { get; set; }
+
+        public DateTime DateMovedIn { get; set; }
     }
 }
