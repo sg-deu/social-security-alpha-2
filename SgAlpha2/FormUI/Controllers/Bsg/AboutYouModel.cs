3442eed7a237a5a60e824f49bec3c243ad2c3784
Richard Brown <richard.brown@incrementalgroup.co.uk>
2017-02-09 11:18:05 +0000

    Added CurrentAddressStatus to BSG model

--- SgAlpha2/FormUI/Controllers/Bsg/AboutYouModel.cs
+++ SgAlpha2/FormUI/Controllers/Bsg/AboutYouModel.cs
@@ -10,35 +10,41 @@ namespace FormUI.Controllers.Bsg
             PreviousAddresses = new List<AddressModel>();
         }
 
-        public string       Title                       { get; set; }
+        public string           Title                       { get; set; }
 
-        public string       FirstName                   { get; set; }
+        public string           FirstName                   { get; set; }
 
-        public string       OtherNames                  { get; set; }
+        public string           OtherNames                  { get; set; }
 
-        public string       SurnameOrFamilyName         { get; set; }
+        public string           SurnameOrFamilyName         { get; set; }
 
-        public DateTime     DateOfBirth                 { get; set; }
+        public DateTime         DateOfBirth                 { get; set; }
 
-        public string       NationalInsuranceNumberText { get; set; }
+        public string           NationalInsuranceNumberText { get; set; }
 
-        public AddressModel CurrentAddress              { get; set; }
+        public AddressModel     CurrentAddress              { get; set; }
 
-        public bool?        CurrentAddressIsPermanent   { get; set; }
+        public AddressStatus?   CurrentAddressStatus   { get; set; }
 
         public IList<AddressModel> PreviousAddresses { get; set; }
     }
 
     public class AddressModel
     {
-        public string   Street1     { get; set; }
+        public string           Street1     { get; set; }
 
-        public string   Street2     { get; set; }
+        public string           Street2     { get; set; }
 
-        public string   TownOrCity  { get; set; }
+        public string           TownOrCity  { get; set; }
 
-        public string   Postcode    { get; set; }
+        public string           Postcode    { get; set; }
 
-        public DateTime DateMovedIn { get; set; }
+        public DateTime         DateMovedIn { get; set; }
+    }
+
+    public enum AddressStatus
+    {
+        Permanent,
+        Temporary,
     }
 }
