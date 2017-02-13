958aefef3fa3ff9cac36169ccc8455075a6f23fe
Richard Brown <richard.brown@incrementalgroup.co.uk>
2017-02-13 16:16:33 +0000

    Let Repository.Init() silently fail until we can get application settings read correctly on Azure

--- SgAlpha2/FormUI/App_Start/GlobalStartup.cs
+++ SgAlpha2/FormUI/App_Start/GlobalStartup.cs
@@ -28,11 +28,18 @@ namespace FormUI.App_Start
 
         protected virtual void InitRepository()
         {
-            // this only work on local machine for now
-            var localDbUri = new Uri("https://localhost:8081");
-            var localDbKey = "C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==";
+            try
+            {
+                // this only work on local machine for now
+                var localDbUri = new Uri("https://localhost:8081");
+                var localDbKey = "C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==";
 
-            Repository.Init(localDbUri, localDbKey);
+                Repository.Init(localDbUri, localDbKey);
+            }
+            catch
+            {
+                // let this silently break until we have this set-up on Azure
+            }
         }
     }
 }
