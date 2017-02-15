7d305ab12faff1daf89bf1a9f3b68bf30df7098c
Richard Brown <richard.brown@incrementalgroup.co.uk>
2017-02-15 15:17:54 +0000

    Hid command window when running PhantomJS

--- SgAlpha2/FormUI.Tests/SystemTests/Util/BrowserApp.cs
+++ SgAlpha2/FormUI.Tests/SystemTests/Util/BrowserApp.cs
@@ -12,9 +12,15 @@ namespace FormUI.Tests.SystemTests.Util
         public BrowserApp(bool runHeadless)
         {
             if (runHeadless)
-                _browser = new PhantomJSDriver();
+            {
+                var service = PhantomJSDriverService.CreateDefaultService();
+                service.HideCommandPromptWindow = true;
+                _browser = new PhantomJSDriver(service);
+            }
             else
+            {
                 _browser = new ChromeDriver();
+            }
         }
 
         public void Dispose()
