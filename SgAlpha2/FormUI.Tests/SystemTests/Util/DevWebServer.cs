a91f3509a469ec113639fbdd3be4ce1d871b8c9a
Richard Brown <richard.brown@incrementalgroup.co.uk>
2017-02-16 10:34:53 +0000

    Added semaphore to prevent two builds on same machine clashing

--- SgAlpha2/FormUI.Tests/SystemTests/Util/DevWebServer.cs
+++ SgAlpha2/FormUI.Tests/SystemTests/Util/DevWebServer.cs
@@ -1,23 +1,65 @@
-﻿using CassiniDev;
+﻿using System;
+using System.Security.AccessControl;
+using System.Threading;
+using CassiniDev;
 
 namespace FormUI.Tests.SystemTests.Util
 {
     public static class DevWebServer
     {
         private static CassiniDevServer _server;
+        private static Semaphore        _enforceSingleInstance;
 
         public static string RootUrl { get; private set; }
 
         public static void Start()
         {
+            var security = new SemaphoreSecurity();
+            security.AddAccessRule(new SemaphoreAccessRule("Everyone", SemaphoreRights.FullControl, AccessControlType.Allow));
+
+            bool createdNew_notUsed;
+            var semaphoreName = "Global\\DevWebServer";
+
+            lock (typeof(DevWebServer))
+                try
+                {
+                    DisposeSemaphore();
+
+                    _enforceSingleInstance = new Semaphore(1, 1, semaphoreName, out createdNew_notUsed, security);
+
+                    if (!_enforceSingleInstance.WaitOne())
+                        throw new Exception("Could not obtain semaphore: " + semaphoreName);
+                }
+                catch
+                {
+                    DisposeSemaphore();
+                    throw;
+                }
+
             _server = new CassiniDevServer();
             _server.StartServer(@"..\..\..\FormUI", 54078, "/", "localhost");
+
             RootUrl = _server.RootUrl;
         }
 
         public static void Stop()
         {
-            _server.StopServer();
+            DisposeSemaphore();
+
+            if (_server != null)
+                _server.StopServer();
+        }
+
+        private static void DisposeSemaphore()
+        {
+            lock (typeof(DevWebServer))
+                if (_enforceSingleInstance != null)
+                {
+                    using (_enforceSingleInstance)
+                        _enforceSingleInstance.Release();
+
+                    _enforceSingleInstance = null;
+                }
         }
     }
 }
