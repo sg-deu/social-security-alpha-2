10c01ad84aa91a393936fbfc3d748a425f0416ff
Richard Brown <richard.brown@incrementalgroup.co.uk>
2017-02-22 11:13:33 +0000

    Maintained scroll position after post (might have to make it jump up if there are validation errors on the page?) AL2-228 - https://ssalpha.atlassian.net/browse/AL2-228

--- SgAlpha2/FormUI/Scripts/formui.js
+++ SgAlpha2/FormUI/Scripts/formui.js
@@ -4,7 +4,19 @@
 
     formui.init = init;
 
-    init($(document));
+    // from:  http://stackoverflow.com/questions/484463/how-do-i-maintain-scroll-position-in-mvc/16209536#16209536
+    $(document).scroll(function () {
+        localStorage['page'] = document.URL;
+        localStorage['scrollTop'] = $(document).scrollTop();
+    });
+
+    if (localStorage['page'] == document.URL) {
+        $(document).scrollTop(localStorage['scrollTop']);
+    }
+
+    $(function () {
+        init($(document));
+    });
 
     function init(rootElement) {
 
