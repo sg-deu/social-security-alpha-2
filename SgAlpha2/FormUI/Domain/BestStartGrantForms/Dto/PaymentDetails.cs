f9a32ffe02dc14369cbe14d4bced3b455e5d57ae
Richard Brown <richard.brown@incrementalgroup.co.uk>
2017-02-23 16:39:45 +0000

    Added max-length UI hints AL2-230 - https://ssalpha.atlassian.net/browse/AL2-230

--- SgAlpha2/FormUI/Domain/BestStartGrantForms/Dto/PaymentDetails.cs
+++ SgAlpha2/FormUI/Domain/BestStartGrantForms/Dto/PaymentDetails.cs
@@ -19,14 +19,17 @@ namespace FormUI.Domain.BestStartGrantForms.Dto
 
         [DisplayName("Sort Code")]
         [HintText("Please tell us all 6 numbers, for example 12-34-56")]
+        [UiLength(8)]
         public string   SortCode            { get; set; }
 
         [DisplayName("Account number")]
         [HintText("Most account numbers are 8 numbers long; if your account number has fewer than 10 numbers, please fill in the numbers from the left")]
+        [UiLength(10)]
         public string   AccountNumber       { get; set; }
 
         [DisplayName("Building Society roll or reference number")]
         [HintText("If you are using a building society account number you may need to tell us a roll or reference number; this may be made up of letters and numbers, and may be up to 18 characters long; if you are not sure if the account has a roll or reference number, ask the building society")]
+        [UiLength(18)]
         public string   RollNumber          { get; set; }
     }
 }
