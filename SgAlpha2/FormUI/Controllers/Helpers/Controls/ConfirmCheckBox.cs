48d35b3bf2588c5ec4790719d4488edd38ecde05
Richard Brown <richard.brown@incrementalgroup.co.uk>
2017-02-28 09:40:19 +0000

    Emphasised confirmation AL2-233 - https://ssalpha.atlassian.net/browse/AL2-233

--- SgAlpha2/FormUI/Controllers/Helpers/Controls/ConfirmCheckBox.cs
+++ SgAlpha2/FormUI/Controllers/Helpers/Controls/ConfirmCheckBox.cs
@@ -4,16 +4,26 @@ namespace FormUI.Controllers.Helpers.Controls
 {
     public class ConfirmCheckBox : InputControl
     {
-        private string _labelText;
+        private string  _labelText;
+        private bool    _emphasise;
 
         public ConfirmCheckBox(ControlContext controlContext, string labelText) : base(controlContext)
         {
             _labelText = labelText;
         }
 
+        public ConfirmCheckBox Emphasise(bool emphasise)
+        {
+            _emphasise = emphasise;
+            return this;
+        }
         protected override HtmlTag CreateTag()
         {
             var container = new DivTag().AddClasses("confirm-check-box");
+
+            if (_emphasise)
+                container.AddClass("emphasise");
+
             var modelValue = RenderValue();
 
             var input = new HtmlTag("input").Attr("type", "checkbox").Name(ControlContext.Name).Value("True");
