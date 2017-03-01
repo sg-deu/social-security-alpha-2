ab38993cb8b5ad7c771b87e6979021f489824742
Richard Brown <richard.brown@incrementalgroup.co.uk>
2017-03-01 15:05:34 +0000

    Added actions for all sections AL2-113 - https://ssalpha.atlassian.net/browse/AL2-113

---
+++ SgAlpha2/FormUI.Tests/Controllers/Bsg/SectionActionStrategyTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using FluentAssertions;
+using FormUI.Controllers.Bsg;
+using FormUI.Domain.BestStartGrantForms;
+using NUnit.Framework;
+
+namespace FormUI.Tests.Controllers.Bsg
+{
+    [TestFixture]
+    public class SectionActionStrategyTests
+    {
+        [Test]
+        public void AllSectionsHaveActions()
+        {
+            var sectionActions = new List<string>();
+
+            foreach (Sections section in Enum.GetValues(typeof(Sections)))
+            {
+                var strategy = SectionActionStrategy.For(section);
+                var action = strategy.Action("formId");
+                action.Should().NotBeNullOrWhiteSpace();
+                action.Should().Contain("formId");
+
+                sectionActions.Contains(action).Should().BeFalse("Action {0} should only be used once", action);
+                sectionActions.Add(action);
+            }
+        }
+    }
+}
--- SgAlpha2/FormUI/Controllers/Bsg/SectionActionStrategy.cs
+++ SgAlpha2/FormUI/Controllers/Bsg/SectionActionStrategy.cs
@@ -1,19 +1,29 @@
 ﻿using System;
+using System.Collections.Generic;
 using FormUI.Domain.BestStartGrantForms;
 
 namespace FormUI.Controllers.Bsg
 {
     public abstract class SectionActionStrategy
     {
+        private static IDictionary<Sections, Func<SectionActionStrategy>> _strategies = new Dictionary<Sections, Func<SectionActionStrategy>>
+        {
+            { Sections.Consent,             () => new ConsentActions() },
+            { Sections.ApplicantDetails,    () => new ApplicantDetailsActions() },
+            { Sections.ExpectedChildren,    () => new ExpectedChildrenActions() },
+            { Sections.ExistingChildren,    () => new ExistingChildrenActions() },
+            { Sections.ApplicantBenefits,   () => new ApplicantBenefitsActions() },
+            { Sections.HealthProfessional,  () => new HealthProfessionalActions() },
+            { Sections.PaymentDetails,      () => new PaymentDetailsActions() },
+            { Sections.Declaration,         () => new DeclarationActions() },
+        };
+
         public static SectionActionStrategy For(Sections section)
         {
-            switch(section)
-            {
-                case Sections.Consent: return new ConsentActions();
+            if (!_strategies.ContainsKey(section))
+                throw new Exception("Unhandled section: " + section);
 
-                default:
-                    throw new Exception("Unhandled section: " + section);
-            }
+            return _strategies[section]();
         }
 
         public abstract string Action(string formId);
@@ -21,6 +31,41 @@ namespace FormUI.Controllers.Bsg
         private class ConsentActions : SectionActionStrategy
         {
             public override string Action(string formId) { return BsgActions.Consent(formId); }
+        }
+
+        private class ApplicantDetailsActions : SectionActionStrategy
+        {
+            public override string Action(string formId) { return BsgActions.ApplicantDetails(formId); }
+        }
+
+        private class ExpectedChildrenActions : SectionActionStrategy
+        {
+            public override string Action(string formId) { return BsgActions.ExpectedChildren(formId); }
+        }
+
+        private class ExistingChildrenActions : SectionActionStrategy
+        {
+            public override string Action(string formId) { return BsgActions.ExistingChildren(formId); }
+        }
+
+        private class ApplicantBenefitsActions : SectionActionStrategy
+        {
+            public override string Action(string formId) { return BsgActions.ApplicantBenefits1(formId); }
+        }
+
+        private class HealthProfessionalActions : SectionActionStrategy
+        {
+            public override string Action(string formId) { return BsgActions.HealthProfessional(formId); }
+        }
+
+        private class PaymentDetailsActions : SectionActionStrategy
+        {
+            public override string Action(string formId) { return BsgActions.PaymentDetails(formId); }
+        }
+
+        private class DeclarationActions : SectionActionStrategy
+        {
+            public override string Action(string formId) { return BsgActions.Declaration(formId); }
         }
     }
 }
