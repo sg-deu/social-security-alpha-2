9dcb83b318107a2e8a9446e2468ae0678058e44e
Richard Brown <richard.brown@incrementalgroup.co.uk>
2017-03-06 13:12:58 +0000

    Added query for presentation to determine whether to show/hide questions AL2-218 - https://ssalpha.atlassian.net/browse/AL2-218

---
+++ SgAlpha2/FormUI/Domain/BestStartGrantForms/Queries/FindApplicantDetailsConfig.cs
@@ -0,0 +1,20 @@
+﻿using FormUI.Domain.BestStartGrantForms.Dto;
+using FormUI.Domain.BestStartGrantForms.Responses;
+using FormUI.Domain.Util;
+
+namespace FormUI.Domain.BestStartGrantForms.Queries
+{
+    public class FindApplicantDetailsConfig : Query<ApplicantDetailsConfig>
+    {
+        public ApplicantDetails ApplicantDetails;
+
+        public override ApplicantDetailsConfig Find()
+        {
+            return new ApplicantDetailsConfig
+            {
+                ShouldAskCareQuestion       = BestStartGrant.ShouldAskCareQuestion(ApplicantDetails),
+                ShouldAskEducationQuestion  = BestStartGrant.ShouldAskEducationQuestion(ApplicantDetails),
+            };
+        }
+    }
+}
