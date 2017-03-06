a5e1e2a6f62e4c9fd78cd6aa8c8089bff1f7cedd
Richard Brown <richard.brown@incrementalgroup.co.uk>
2017-03-06 16:37:21 +0000

    Pushed enforcement of single test running at any one time up to SetUpFixture (and out of DevWebServer)

---
+++ SgAlpha2/FormUI.Tests/SetUpFixture.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Security.AccessControl;
+using System.Threading;
+using NUnit.Framework;
+
+namespace FormUI.Tests
+{
+    [SetUpFixture]
+    public class SetUpFixture
+    {
+        private static Semaphore _enforceSingleInstance;
+
+        [SetUp]
+        public void SetUp()
+        {
+            Console.Write("Waiting for existing test run to complete ... ");
+
+            try
+            {
+                var security = new SemaphoreSecurity();
+                security.AddAccessRule(new SemaphoreAccessRule("Everyone", SemaphoreRights.FullControl, AccessControlType.Allow));
+
+                bool createdNew_notUsed;
+                var semaphoreName = "Global\\FormUITests";
+
+                lock (typeof(SetUpFixture))
+                {
+                    DisposeSemaphore();
+
+                    _enforceSingleInstance = new Semaphore(1, 1, semaphoreName, out createdNew_notUsed, security);
+
+                    if (!_enforceSingleInstance.WaitOne(TimeSpan.FromSeconds(180)))
+                        throw new Exception("Could not obtain semaphore: " + semaphoreName);
+                }
+
+                Console.WriteLine("existing tests complete");
+            }
+            catch
+            {
+                TearDown();
+                throw;
+            }
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            DisposeSemaphore();
+        }
+
+        private static void DisposeSemaphore()
+        {
+            lock (typeof(SetUpFixture))
+                if (_enforceSingleInstance != null)
+                {
+                    using (_enforceSingleInstance)
+                        _enforceSingleInstance.Release();
+
+                    _enforceSingleInstance = null;
+                }
+        }
+    }
+}
--- SgAlpha2/FormUI.Tests/SystemTests/Util/DevWebServer.cs
+++ SgAlpha2/FormUI.Tests/SystemTests/Util/DevWebServer.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Security.AccessControl;
-using System.Threading;
+﻿using System.Security.AccessControl;
 using CassiniDev;
 
 namespace FormUI.Tests.SystemTests.Util
@@ -8,7 +6,6 @@ namespace FormUI.Tests.SystemTests.Util
     public static class DevWebServer
     {
         private static CassiniDevServer _server;
-        private static Semaphore        _enforceSingleInstance;
 
         public static string RootUrl { get; private set; }
 
@@ -16,25 +13,6 @@ namespace FormUI.Tests.SystemTests.Util
         {
             var security = new SemaphoreSecurity();
             security.AddAccessRule(new SemaphoreAccessRule("Everyone", SemaphoreRights.FullControl, AccessControlType.Allow));
-
-            bool createdNew_notUsed;
-            var semaphoreName = "Global\\DevWebServer";
-
-            lock (typeof(DevWebServer))
-                try
-                {
-                    DisposeSemaphore();
-
-                    _enforceSingleInstance = new Semaphore(1, 1, semaphoreName, out createdNew_notUsed, security);
-
-                    if (!_enforceSingleInstance.WaitOne(TimeSpan.FromSeconds(40)))
-                        throw new Exception("Could not obtain semaphore: " + semaphoreName);
-                }
-                catch
-                {
-                    DisposeSemaphore();
-                    throw;
-                }
 
             _server = new CassiniDevServer();
             _server.StartServer(@"..\..\..\FormUI", 54078, "/", "localhost");
@@ -44,22 +22,8 @@ namespace FormUI.Tests.SystemTests.Util
 
         public static void Stop()
         {
-            DisposeSemaphore();
-
             if (_server != null)
                 _server.StopServer();
-        }
-
-        private static void DisposeSemaphore()
-        {
-            lock (typeof(DevWebServer))
-                if (_enforceSingleInstance != null)
-                {
-                    using (_enforceSingleInstance)
-                        _enforceSingleInstance.Release();
-
-                    _enforceSingleInstance = null;
-                }
         }
     }
 }
