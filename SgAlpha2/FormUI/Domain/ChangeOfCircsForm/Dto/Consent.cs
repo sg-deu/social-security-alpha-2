e2239aabd1741db96d6e359164e4b449862b3883
Richard Brown <richard.brown@incrementalgroup.co.uk>
2017-03-21 13:01:45 +0000

    Updated consent text to include CoC AL2-362 - https://ssalpha.atlassian.net/browse/AL2-362

--- SgAlpha2/FormUI/Domain/ChangeOfCircsForm/Dto/Consent.cs
+++ SgAlpha2/FormUI/Domain/ChangeOfCircsForm/Dto/Consent.cs
@@ -4,7 +4,7 @@ namespace FormUI.Domain.ChangeOfCircsForm.Dto
 {
     public class Consent
     {
-        [DisplayName("I agree that the Social Security Agency can use the data I have provided in this BSG application form for the purposes of processing my application as described above")]
+        [DisplayName("I agree that the Social Security Agency can use the data I have provided in this BSG/Change of Circumstances application form for the purposes of processing my application as described above")]
         public bool AgreedToConsent { get; set; }
     }
 }
