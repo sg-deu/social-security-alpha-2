f45f47ee57ab3ec974a3e1fa1fa46d65242a2ac3
Richard Brown <richard.brown@incrementalgroup.co.uk>
2017-03-22 16:43:36 +0000

    Added initial example of storing a blob in the local storage emulator (tests explicit for now until we can read the file back out, and configure the 'real' storage service) AL2-304 - https://ssalpha.atlassian.net/browse/AL2-304

---
+++ SgAlpha2/FormUI.Tests/Domain/Util/BlobStoreTests.cs
@@ -0,0 +1,21 @@
+﻿using System.Text;
+using FormUI.Domain.Util;
+using NUnit.Framework;
+
+namespace FormUI.Tests.Domain.Util
+{
+    [TestFixture]
+    [Explicit("experimenting with Azure Blob storage")]
+    public class BlobStoreTests : DomainTest
+    {
+        [Test]
+        public void Store()
+        {
+            BlobStore.Init("UseDevelopmentStorage=true;", "unittest");
+
+            var content = Encoding.ASCII.GetBytes("my file contents");
+
+            BlobStore.Store("file.txt", content);
+        }
+    }
+}
---
+++ SgAlpha2/FormUI/Domain/Util/BlobStore.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+
+namespace FormUI.Domain.Util
+{
+    public class BlobStore
+    {
+        private static CloudBlobClient      _blobClient;
+        private static CloudBlobContainer   _container;
+
+        public static void Init(string connectionString, string containerName)
+        {
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(connectionString);
+            _blobClient = storageAccount.CreateCloudBlobClient();
+
+            _container = _blobClient.GetContainerReference(containerName);
+            _container.CreateIfNotExists();
+        }
+
+        public static void Store(string filename, byte[] content)
+        {
+            var block = _container.GetBlockBlobReference(filename);
+            block.UploadFromByteArray(content, 0, content.Length);
+        }
+    }
+}
