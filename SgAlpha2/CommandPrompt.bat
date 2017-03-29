27d9f2f372d869006dbae1a675ff694480dbd75e
Richard Brown <richard.brown@incrementalgroup.co.uk>
2017-03-29 11:53:05 +0100

    Changed Sg to capitals

--- SgAlpha2/CommandPrompt.bat
+++ SgAlpha2/CommandPrompt.bat
@@ -1,5 +1,5 @@
 @CD /D "%~dp0"
-@title SgAlpha2 Command Prompt
+@title SGAlpha2 Command Prompt
 @SET PATH=C:\Program Files (x86)\MSBuild\14.0\Bin\;%PATH%
 @doskey b=msbuild $* SgAlpha2.proj
 @doskey bPackage=msbuild /p:Configuration=Release /p:DeployOnBuild=true /p:WebPublishMethod=Package /p:PackageAsSingleFile=true /p:SkipInvalidConfigurations=true /p:PackageLocation="%CD%\azurePackage" SgAlpha2.sln
